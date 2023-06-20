90c12e0b50e44fa1155cab6c0b664312a5f33c38
promptdrake <95160973+promptdrake@users.noreply.github.com>
2023-06-20 21:35:28 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const fileUpload = require('express-fileupload');
 const ejs = require('ejs')
 const path = require('path');
-
+const axios = require('axios')
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static('public'));
@@ -31,6 +31,32 @@ const s3Poolenamsatu = new S3Client({
   credentials: {
     accessKeyId: '563d904a8cab73927e4d065be686c431',
     secretAccessKey: '8635b4be8473367947e2acb796823c9f0d8dcd2fd112df16a7ff705b9ec40784'
+  }
+});
+const s3Poolwestern1 = new S3Client({
+  endpoint: 'https://a56f1dbed28e4ef6ad837478a3d3917b.r2.cloudflarestorage.com/poolwestern1',
+  region: 'weur',
+  credentials: {
+    accessKeyId: 'd6445c32810ba064a1558d01282e782d',
+    secretAccessKey: '10b0115a4a607e1d2319d988bea61c6bc09c7634d4692cef596a6254a9e4b134'
+  }
+});
+
+const s3Pooleeur1 = new S3Client({
+  endpoint: 'https://a56f1dbed28e4ef6ad837478a3d3917b.r2.cloudflarestorage.com/eeursatu',
+  region: 'eeur',
+  credentials: {
+    accessKeyId: 'd6445c32810ba064a1558d01282e782d',
+    secretAccessKey: '10b0115a4a607e1d2319d988bea61c6bc09c7634d4692cef596a6254a9e4b134'
+  }
+});
+
+const s3Wnamsatu = new S3Client({
+  endpoint: 'https://a56f1dbed28e4ef6ad837478a3d3917b.r2.cloudflarestorage.com/poolwnamsatu',
+  region: 'wnam',
+  credentials: {
+    accessKeyId: 'd6445c32810ba064a1558d01282e782d',
+    secretAccessKey: '10b0115a4a607e1d2319d988bea61c6bc09c7634d4692cef596a6254a9e4b134'
   }
 });
 
@@ -132,6 +158,101 @@ app.post('/upload/enamsatu', async (req, res) => {
   }
 });
 
+app.post('/upload/weursatu', async (req, res) => {
+  try {
+    if (!req.files || !req.files.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const file = req.files.file;
+    if (!isImage(file)) {
+      return res.status(400).json({ error: 'Nice Try kid for inspecting element and change the accept form only image to everything, Please follow the rules' });
+    }
+
+    const fileExtension = path.extname(file.name);
+
+    const uploadParams = {
+      Bucket: 'poolwestern1',
+      Key: `temp-${Date.now().toString()}${fileExtension}`,
+      Body: file.data,
+      ACL: 'public-read',
+      ContentType: file.mimetype
+    };
+
+    const putObjectCommand = new PutObjectCommand(uploadParams);
+    await s3Poolwestern1.send(putObjectCommand);
+
+    const fileUrl = `https://pub-a368c17f5c404d088a8cf253d09c0cbe.r2.dev/poolwestern1/${uploadParams.Key}`;
+    res.redirect(fileUrl);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while uploading the file' });
+  }
+});
+app.post('/upload/eeursatu', async (req, res) => {
+  try {
+    if (!req.files || !req.files.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const file = req.files.file;
+    if (!isImage(file)) {
+      return res.status(400).json({ error: 'Nice Try kid for inspecting element and change the accept form only image to everything, Please follow the rules' });
+    }
+
+    const fileExtension = path.extname(file.name);
+
+    const uploadParams = {
+      Bucket: 'eeursatu',
+      Key: `temp-${Date.now().toString()}${fileExtension}`,
+      Body: file.data,
+      ACL: 'public-read',
+      ContentType: file.mimetype
+    };
+
+    const putObjectCommand = new PutObjectCommand(uploadParams);
+    await s3Pooleeur1.send(putObjectCommand);
+
+    const fileUrl = `https://pub-5cdfd4d7774e49aa99f6878c6ebe8db9.r2.dev/eeursatu/${uploadParams.Key}`;
+    res.redirect(fileUrl);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while uploading the file' });
+  }
+});
+
+app.post('/upload/wnamsatu', async (req, res) => {
+  try {
+    if (!req.files || !req.files.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const file = req.files.file;
+    if (!isImage(file)) {
+      return res.status(400).json({ error: 'Nice Try kid for inspecting element and change the accept form only image to everything, Please follow the rules' });
+    }
+
+    const fileExtension = path.extname(file.name);
+
+    const uploadParams = {
+      Bucket: 'poolwnamsatu',
+      Key: `temp-${Date.now().toString()}${fileExtension}`,
+      Body: file.data,
+      ACL: 'public-read',
+      ContentType: file.mimetype
+    };
+
+    const putObjectCommand = new PutObjectCommand(uploadParams);
+    await s3Wnamsatu.send(putObjectCommand);
+
+    const fileUrl = `https://pub-f103919a71a647deb68deeec70f967c3.r2.dev/poolwnamsatu/${uploadParams.Key}`;
+    res.redirect(fileUrl);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while uploading the file' });
+  }
+});
+
 function isImage(file) {
   return file.mimetype.startsWith('image/');
 }
@@ -146,7 +267,7 @@ app.get('/', (req, res) => {
   }
 });
 function getRandomOption() {
-  const options = ['poolsatu', 'pooldua', 'enamsatu'];
+  const options = ['poolsatu', 'pooldua', 'enamsatu', 'weursatu', 'eeursatu', 'wnamsatu'];
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 }
